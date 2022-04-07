b0acffeb75ffeb3570d1ad6043315df3b96481f9
fiin <fiin@gmail.com>
2022-04-07 22:01:38 +0700

    chika

---
+++ settings.js
@@ -0,0 +1,59 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6289519241989','6289519241989','6289519241989']
+global.premium = ['6289519241989']
+global.ownername = 'Fiin'
+global.botname = 'FiinBotz'
+global.footer = 'FiinBotz'
+global.ig = 'https://instagram.com/Fiin24'
+global.email = 'elfinpraza@gmail.com'
+global.region = 'Indonesia'
+global.sc = 'Note'
+global.myweb = 'https://api-riychdwayne.herokuap.com'
+global.packname = 'Fiin'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'chika'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./image/chika.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
